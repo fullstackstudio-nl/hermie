331197aba3a07c55ba79095695d2fe3c6758c6c5
Sebastiaan Eekhof <sebastiaan@eekhof.dev>
2026-09-22 17:33:51 +0200

    chore(release): 0.1.6

--- apps/hermie/app.config.ts
+++ apps/hermie/app.config.ts
@@ -50,7 +50,7 @@ const config: ExpoConfig = {
   name: 'Hermie',
   slug: 'hermie',
   owner: 'fullstack-studio',
-  version: '0.1.5',
+  version: '0.1.6',
   orientation: 'default',
   scheme: 'hermie',
   userInterfaceStyle: 'automatic',
